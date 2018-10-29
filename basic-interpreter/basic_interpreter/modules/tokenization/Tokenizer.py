4559150057e951608cc5dbbbab852877a3fef689
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-10-29 12:01:39 -0300

    Start tokenization of numbers and multi-char specials

--- basic-interpreter/basic_interpreter/modules/tokenization/Tokenizer.py
+++ basic-interpreter/basic_interpreter/modules/tokenization/Tokenizer.py
@@ -1,32 +1,62 @@
 from basic_interpreter.modules.EventDrivenModule import EventDrivenModule
 
+COMPOSED_SPECIAL = {'>=', '<='}
+
 
 class Tokenizer(EventDrivenModule):
     def get_handlers(self):
         self.character_queue = []
+        self.special = ''
         return {
             'ascii_character': self.ascii_character_handler,
             'ascii_digit': self.ascii_digit_handler,
-            'ascii_delimiter': self.ascii_delimiter_or_ctrl_handler,
-            'ascii_ctrl': self.ascii_delimiter_or_ctrl_handler,
+            'ascii_delimiter': self.ascii_delimiter_handler,
+            'ascii_ctrl': self.ascii_ctrl_handler,
             'ascii_special': self.ascii_special_handler,
         }
 
     def ascii_character_handler(self, event):
+        self.end_of_special()
         assert not self.character_queue or self.character_queue[0].isalpha(), 'character found in a number'
         self.character_queue.append(event[0])
 
     def ascii_digit_handler(self, event):
+        if self.special in {'+', '-'}:
+            self.character_queue.append(self.special)
+            self.special = ''
+        else:
+            self.end_of_special()
         self.character_queue.append(event[0])
 
-    def ascii_delimiter_or_ctrl_handler(self, event):
+    def end_of_identifier(self):
         if self.character_queue:
-            self.add_external_event(('identifier', ''.join(self.character_queue)))
+            if self.character_queue[0].isalpha():
+                self.add_external_event(('token_identifier', ''.join(self.character_queue)))
+            else:
+                self.add_external_event(('token_number', int(''.join(self.character_queue))))
             self.character_queue = []
 
-    def ascii_special_handler(self, event):
-        self.ascii_delimiter_or_ctrl_handler(event)
-        self.add_external_event(('special', event[0]))
+    def end_of_special(self, event=''):
+        if self.special + event in COMPOSED_SPECIAL:
+            self.add_external_event(('token_special', self.special + event))
+            self.special = ''
+        else:
+            if self.special:
+                self.add_external_event(('token_special', self.special))
+            self.special = event
 
-    # TODO numbers
-    # TODO multi-char special (e.g. :=)
+    def ascii_delimiter_handler(self, event):
+        self.end_of_identifier()
+        self.end_of_special()
+
+    def ascii_ctrl_handler(self, event):
+        self.end_of_identifier()
+        self.end_of_special()
+        self.add_external_event(('token_ctrl', event[0]))
+
+    def ascii_special_handler(self, event):
+        self.end_of_identifier()
+        if self.special:
+            self.end_of_special(event[0])
+        else:
+            self.special = event[0]
--- basic-interpreter/tests/modules/tokenization/test_Tokenizer.py
+++ basic-interpreter/tests/modules/tokenization/test_Tokenizer.py
@@ -1,22 +1,47 @@
 from pathlib import Path
-
 from unittest.mock import call, MagicMock
+
+import pytest
 
 from basic_interpreter.modules.tokenization.AsciiCategorizer import AsciiCategorizer
 from basic_interpreter.modules.tokenization.Tokenizer import Tokenizer
 
 
-def test_filters_ascii_chars():
+@pytest.mark.parametrize('source_line,tokens', [
+    (('ascii_line', '1 IF S(P) = 0 THEN GOTO 3\n'), [
+        call(('token_number', 1)),
+        call(('token_identifier', 'IF')),
+        call(('token_identifier', 'S')),
+        call(('token_special', '(')),
+        call(('token_identifier', 'P')),
+        call(('token_special', ')')),
+        call(('token_special', '=')),
+        call(('token_number', 0)),
+        call(('token_identifier', 'THEN')),
+        call(('token_identifier', 'GOTO')),
+        call(('token_number', 3)),
+        call(('token_ctrl', '\n')),
+    ]),
+    (('ascii_line', '1 IF S2(P) <= -5 THEN GOTO 3\n'), [
+        call(('token_number', 1)),
+        call(('token_identifier', 'IF')),
+        call(('token_identifier', 'S2')),
+        call(('token_special', '(')),
+        call(('token_identifier', 'P')),
+        call(('token_special', ')')),
+        call(('token_special', '<=')),
+        call(('token_number', -5)),
+        call(('token_identifier', 'THEN')),
+        call(('token_identifier', 'GOTO')),
+        call(('token_number', 3)),
+        call(('token_ctrl', '\n')),
+    ]),
+])
+def test_filters_ascii_chars(source_line, tokens):
     add_external_event = MagicMock()
     tokenizer = Tokenizer(add_external_event)
     categorizer = AsciiCategorizer(tokenizer.handle_event)
-    categorizer.handle_event(('ascii_line', '1 IF S(P) = 0 THEN GOTO 3\n'))
+    categorizer.handle_event(source_line)
     for event in categorizer:
         categorizer.handle_event(event)
-    for event in tokenizer:
-        print(event)
-        tokenizer.handle_event(event)
-    # WIP
-    # add_external_event.assert_has_calls([
-    #     call()
-    # ])
+    add_external_event.assert_has_calls(tokens)

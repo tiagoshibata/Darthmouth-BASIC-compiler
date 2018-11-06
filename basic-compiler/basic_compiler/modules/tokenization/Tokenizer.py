935dcd0c5738736dfade8bf2a56d8515653a1f47
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-11-05 23:18:44 -0200

    Create transition table for lexer FSM

--- basic-compiler/basic_compiler/modules/tokenization/Tokenizer.py
+++ basic-compiler/basic_compiler/modules/tokenization/Tokenizer.py
@@ -1,6 +1,53 @@
 from basic_compiler.modules.EventDrivenModule import EventDrivenModule
 
-COMPOSED_SPECIAL = {'>=', '<='}
+TRANSITION_TABLE = {
+    'start': (False, [
+        ('ascii_character', 'identifier'),
+        ('ascii_digit', 'number'),
+        ('ascii_delimiter', 'start'),
+        ('ascii_ctrl', 'end_of_line'),
+        (('ascii_special', '>'), 'greater_than'),
+        (('ascii_special', '<'), 'smaller_than'),
+        ('ascii_special', 'special'),
+    ]),
+
+    'identifier': (True, [
+        ('ascii_character', 'identifier'),
+        ('ascii_digit', 'identifier'),
+    ]),
+
+    'number': (True, [
+        (('ascii_character', 'E'), 'scientific_notation_number'),
+        ('ascii_character', None),
+        ('ascii_digit', 'number'),
+    ]),
+    'scientific_notation_number': (False, [
+        (('ascii_character', '+'), 'scientific_notation_number_exponent_after_sign'),
+        (('ascii_character', '-'), 'scientific_notation_number_exponent_after_sign'),
+        ('ascii_digit', 'scientific_notation_number_exponent_value'),
+    ]),
+    'scientific_notation_number_exponent_after_sign': (False, [
+        ('ascii_digit', 'scientific_notation_number_exponent_value'),
+    ]),
+    'scientific_notation_number_exponent_value': (True, [
+        ('ascii_digit', 'scientific_notation_number_exponent_value'),
+    ]),
+
+    'end_of_line': True,
+
+    # Multicharacter specials
+    'greater_than': (True, [
+        (('ascii_special', '='), 'greater_or_equal_than'),
+    ]),
+    'greater_or_equal_than': True,
+    'smaller_than': (True, [
+        (('ascii_special', '='), 'smaller_or_equal_than'),
+        (('ascii_special', '>'), 'different'),
+    ]),
+    'smaller_or_equal_than': True,
+    'different': True,
+    'special': True,
+}
 
 
 def is_numeric_prefix(s):

d4320210fb3d6242cad0d36911c2d1a6d2e1ba9f
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-12-17 20:37:00 -0200

    Set graphviz font size, add syntax recognizer plot

--- basic-compiler/basic_compiler/scripts/plot_automata.py
+++ basic-compiler/basic_compiler/scripts/plot_automata.py
@@ -1,44 +1,45 @@
 '''Based on https://graphviz.gitlab.io/_pages/Gallery/directed/fsm.html'''
-import argparse
-
-from basic_compiler.modules.EventEngine import EventEngine
-from basic_compiler.modules.syntax_recognizer.SyntaxRecognizer import SyntaxRecognizer
 from basic_compiler.modules.tokenization import Tokenizer
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description='Plot automata to file.')
-    return parser.parse_args()
+from basic_compiler.modules.syntax_recognizer.SyntaxRecognizer import SyntaxRecognizer
 
 TEMPLATE = '''digraph finite_state_machine {{
     rankdir=LR;
     size="8,5"
     node [shape = point]; starting_point;
     {states}
-    node [shape = circle, label="\E"];
     starting_point -> start;
     {transitions}
 }}'''
 
 
-def main(args):
+def fsm_to_graphviz(fsm_dict):
     states = []
     transitions = []
-    for name, state in Tokenizer.TRANSITION_TABLE.items():
+    for name, state in fsm_dict.items():
         if state.accept:
-            states.append('node [shape = doublecircle, label="{name} (token class \\"{token}\\")"] {name};'.format(name=name, token=state.accept))
+            states.append('node [shape = doublecircle, label="{name} (token class \\"{token}\\")", fontsize=28] {name};'.format(name=name, token=state.accept))
         else:
-            states.append('node [shape = circle, label={name}] {name};'.format(name=name))
+            states.append('node [shape = circle, label="{name}", fontsize=28] "{name}";'.format(name=name))
         if state.transitions:
             for transition in state.transitions:
                 event = str(transition.event).replace('"', '\\"')
-                transitions.append('{} -> {} [ label = "{}" ];'.format(name, transition.to, event))
+                transitions.append('"{}" -> "{}" [ label = "{}", fontsize=28 ];'.format(name, transition.to, event))
 
-    with open('tokenizer.gv', 'w') as f:
-        f.write(TEMPLATE.format(
+    return TEMPLATE.format(
             states='\n'.join(states),
             transitions='\n'.join(transitions),
-        ))
+        )
+
+def main():
+    tokenizer_graphviz = fsm_to_graphviz(Tokenizer.TRANSITION_TABLE)
+    with open('tokenizer.gv', 'w') as f:
+        f.write(tokenizer_graphviz)
+
+    recognizer = SyntaxRecognizer()
+    recognizer.open_handler([''])
+    recognizer_graphviz = fsm_to_graphviz(recognizer.fsm.states)
+    with open('recognizer.gv', 'w') as f:
+        f.write(recognizer_graphviz)
 
 if __name__ == '__main__':
-    main(parse_args())
+    main()
